4089812f6a15cf26eea6e229d0345f95143f12fd
Alexis Carrada <carradaalexis551@gmail.com>
2025-08-20 18:40:42 -0600

    Agregando data y conociendo props y el Set

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,12 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Guitar from "./components/Guitar";
 import Header from "./components/Header";
-
+import { db } from "./data/db";
 function App() {
   // State
 
-  const [auth, setAuth] = useState(false);
-
-  useEffect(() => {
-    if (auth) {
-      console.log("Autenticado");
-    }
-  }, [auth]);
-
-  setTimeout(() => {
-    setAuth(true);
-  }, 3000);
+  const [data, setData] = useState(db);
+  const [cart, setCart] = useState([]);
 
   return (
     <>
@@ -25,12 +16,9 @@ function App() {
         <h2 className="text-center">Nuestra Colección</h2>
 
         <div className="row mt-5">
-          <Guitar />
-          <Guitar />
-          <Guitar />
-          <Guitar />
-          <Guitar />
-          <Guitar />
+          {data.map((guitar) => (
+            <Guitar key={guitar.id} guitar={guitar} setCart={setCart} />
+          ))}
         </div>
       </main>
 
